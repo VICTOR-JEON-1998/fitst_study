e0d1dfb52f552212466874538c4d355d06fa1a99
VICTOR-JEON-1998 <jeonseongcheol@naver.com>
2025-01-30 17:41:58 +0900

    로그인 기본 화면

--- lib/common/component/custom_text_form_field.dart
+++ lib/common/component/custom_text_form_field.dart
@@ -2,12 +2,19 @@ import 'package:first/common/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
+  // 외부에서 별도로 textField 마다 따로 정의해야 하는 것들은 파라미터로 정의한다.
   final String? hintText; // null 값 가능 처리
   final String? errorText;
+  final bool obscureTexts;
+  final bool autoFocus;
+  final ValueChanged<String>? onChanged;
 
   const CustomTextFormField({
+    required this.onChanged,
     this.hintText,
     this.errorText,
+    this.obscureTexts = false,
+    this.autoFocus = false,
     Key? key}) : super(key: key);
 
   @override
@@ -21,9 +28,10 @@ class CustomTextFormField extends StatelessWidget {
 
     return TextFormField(
       cursorColor: PRIMARY_COLOR,
-      //비밀번호 입력할 때
-      obscureText: true,
-      autofocus: false,
+      //비밀번호 입력할 때만 사용함
+      obscureText: obscureTexts, // true ->글자 작성시 별표처리 형
+      autofocus: autoFocus, // 화면에 cursor 들어오면 자동으로 포커스
+      onChanged: onChanged,
       decoration: InputDecoration( // Text필드 안에서 추가로 패딩을 적용할 떄는 InputDecoration을 사용하면 됨
         contentPadding: EdgeInsets.all(20),
         hintText: hintText, // Text 입력 전에 보이는 글자.
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,15 @@ class _App extends StatelessWidget{
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CustomTextFormField(hintText: "이메일을 입력하세요",),
+            CustomTextFormField(
+              hintText: "이메일을 입력하세요",
+              onChanged: (String value) {},
+            ),
+            CustomTextFormField(
+              hintText: "비밀번호를 입력하세요",
+              onChanged: (String value) {},
+              obscureTexts: true,
+            ),
           ],
         )
       ),
